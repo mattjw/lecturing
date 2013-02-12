55f0f305d5d4c53c6b0fb44b91d0cb627db6ce5b
Matt J Williams <mattjw@mattjw.net>
2013-02-12 15:24:08 +0000

    Update to grader. Now prints out line number of failure.

--- JUnit Autograder/CSIJUnitGrader.java
+++ JUnit Autograder/CSIJUnitGrader.java
@@ -1,6 +1,10 @@
 /*
  * Author: Matt J Williams
  * Created: February 2013
+ *
+ * Version: 12 Feb 2013
+ * Updates:
+ * * Failure cause now includes the line number where the test failed.
  */
 
 
@@ -12,23 +16,23 @@ import java.lang.reflect.*;
 
  
 /*
-    Java app that will run a class containing JUnit tests, and assign a grade
-    based on how many tests passed.
-    
-    Command line args:
-    CSIJunitGrader [-s] className
-    
-    className: the name of the class containing unit tests
-    -s:        output the score only
-    
-    For grading, the test class should include fields indicating the maximum 
-    number of marks available and the marks that each test is worth.
-    In particular, the class should have a "maximumMark" field with the maximum 
-    achievable mark for the assignment, and a field accompanying each test 
-    method specifying the marks for that test. A companion field must have the
-    same name as the test method it accompanies. 
-    
-    Fields representing marks should be static floats.
+ * Java app that will run a class containing JUnit tests, and assign a grade
+ * based on how many tests passed.
+ *  
+ * Command line args:
+ * CSIJunitGrader [-s] className
+ * 
+ * className: the name of the class containing unit tests
+ * -s:        output the score only
+ * 
+ * For grading, the test class should include fields indicating the maximum 
+ * number of marks available and the marks that each test is worth.
+ * In particular, the class should have a "maximumMark" field with the maximum 
+ * achievable mark for the assignment, and a field accompanying each test 
+ * method specifying the marks for that test. A companion field must have the
+ * same name as the test method it accompanies. 
+ * 
+ * Fields representing marks should be static floats.
  */
 public class CSIJUnitGrader
 {
@@ -57,6 +61,24 @@ public class CSIJUnitGrader
             System.exit(1);
         }
         return 0;
+    }
+    
+    /*
+     * Look through a stack trace and find the line where an error occurred
+     * in a particular class file.
+     */ 
+    public static int getExceptionLineNumber( StackTraceElement[] trace, String classFileName )
+    {
+        for( int i=0; i < trace.length; i++ )
+        {
+            StackTraceElement elem = trace[i];
+            String errClass = elem.getClassName();
+            if( errClass.contains( classFileName ) )
+            {
+                return elem.getLineNumber();
+            }
+        }
+        return -1;
     }
     
     public static void main( String[] args )
@@ -146,8 +168,17 @@ public class CSIJUnitGrader
                 float testMarks = getFieldFloatValue( junitClass, testName );
                 
                 System.out.println( "Failed test name: " + testName );
-                System.out.println( "Failure cause: " + f.getMessage() );
+                System.out.println( "Failure cause: ");
+                System.out.println( "\t  " + f.getMessage() );
+                System.out.printf( "\t  see Line %s of %s%n", getExceptionLineNumber( f.getException().getStackTrace(), junitClassName ), junitClassName );
+                //System.out.println( "\t" + f.getException() );
+                //System.out.println( "\t" + f.getDescription() );
+                //System.out.println( "\t" + f.getTrace() );
+                //System.out.println( "-\t" + f.getException().getStackTrace() );
                 System.out.println( "Marks deducted: " + testMarks );
+                
+                //int linenum = getExceptionLineNumber( f.getException().getStackTrace(), junitClassName );
+                
                 System.out.println();
             }
         
